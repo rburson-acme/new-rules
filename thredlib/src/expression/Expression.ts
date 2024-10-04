11fce411b70cfc06662d62c84826040b0a7c5217
Rob Burson <rburson@gmail.com>
2024-10-04 14:57:47 -0400

    moved much of engine core to async code

--- thredlib/src/expression/Expression.ts
+++ thredlib/src/expression/Expression.ts
@@ -3,37 +3,38 @@ import { Event } from '../core/Event.js';
 import { Logger } from '../lib/Logger.js';
 import { defaultBindings } from './defaultBindings.js';
 import { ExpressionContext } from './ExpressionContext.js';
+import { ResourceLockedError } from 'redlock';
 
-export type ExpressionParams = { event: Event, context: ExpressionContext }
+export type ExpressionParams = { event: Event; context: ExpressionContext };
 
 export class Expression {
+  private readonly expression: jsonata.Expression;
 
-    private readonly expression: jsonata.Expression;
-
-    constructor(expr: string) {
-        try {
-            this.expression = jsonata(expr);
-        } catch (e: any) {
-            const msg = `Invalid Expr: '${expr}' :: ${e.message}`;
-            Logger.warn(msg);
-            throw new Error(msg);
-        }
+  constructor(expr: string) {
+    try {
+      this.expression = jsonata(expr);
+    } catch (e: any) {
+      const msg = `Invalid Expr: '${expr}' :: ${e.message}`;
+      Logger.warn(msg);
+      throw new Error(msg);
     }
+  }
 
-    apply(params: ExpressionParams, bindings: Record<string, string> = {}) {
-            try {
-                return this.expression.evaluate(params, this.bindingSetup(bindings, params));
-            } catch (e: any) {
-                const msg = `apply failed while evaluating expr: '${this.expression}' :: ${e.message}`;
-                Logger.warn(msg);
-                throw new Error(msg);
-            }
+  async apply(params: ExpressionParams, bindings: Record<string, string> = {}): Promise<any> {
+    try {
+      // await and catch any errors before returning
+      return await this.expression.evaluate(params, this.bindingSetup(bindings, params));
+    } catch (e: any) {
+      const msg = `apply failed while evaluating expr: '${this.expression}' :: ${e.message}`;
+      Logger.warn(msg);
+      throw new Error(msg);
     }
+  }
 
-    bindingSetup(bindings: Record<string, string>, params: ExpressionParams) {
-        return {
-            ...defaultBindings(params),
-            ...bindings,
-        };
-    }
+  bindingSetup(bindings: Record<string, string>, params: ExpressionParams) {
+    return {
+      ...defaultBindings(params),
+      ...bindings,
+    };
+  }
 }
--- thredlib/src/expression/Transformer.ts
+++ thredlib/src/expression/Transformer.ts
@@ -1,34 +1,39 @@
+import { Series } from '../lib/Async.js';
 import { Expression, ExpressionParams } from './Expression.js';
 
 export class Transformer {
+  static prefix: string = '$xpr';
 
-    static prefix: string = '$xpr';
-
-    static transformObject(source: any, expressionParams: ExpressionParams): any {
-        if (source === null || source === undefined) {
-            return undefined;
-        }
-        if (Array.isArray(source)) {
-            return source.map(p => Transformer.transformObject(p, expressionParams));
-        } if (typeof source === 'object') {
-            let target: any = {};
-            Object.keys(source).forEach((key) => {
-                if (key === Transformer.prefix) {
-                    target = {
-                        ...target,
-                        ...Transformer.transformObject(new Expression(source[key]).apply(expressionParams), expressionParams),
-                    };
-                } else {
-                    target[key] = Transformer.transformObject(source[key], expressionParams);
-                }
-            });
-            return target;
-        } if (typeof source === 'string') {
-            if (source.startsWith(`${Transformer.prefix}(`)) {
-                const expr = source.substring(Transformer.prefix.length + 1, source.length - 1).trim();
-                return Transformer.transformObject(new Expression(expr).apply(expressionParams), expressionParams);
-            }
-        }
-        return source;
+  static async transformObject(source: any, expressionParams: ExpressionParams): Promise<any> {
+    if (source === null || source === undefined) {
+      return undefined;
     }
+    if (Array.isArray(source)) {
+      return await Series.map(source, (p) => Transformer.transformObject(p, expressionParams));
+    }
+    if (typeof source === 'object') {
+      let target: any = {};
+      await Series.forEach(Object.keys(source), async (key) => {
+        if (key === Transformer.prefix) {
+          target = {
+            ...target,
+            ...(await Transformer.transformObject(
+              await new Expression(source[key]).apply(expressionParams),
+              expressionParams,
+            )),
+          };
+        } else {
+          target[key] = await Transformer.transformObject(source[key], expressionParams);
+        }
+      });
+      return target;
+    }
+    if (typeof source === 'string') {
+      if (source.startsWith(`${Transformer.prefix}(`)) {
+        const expr = source.substring(Transformer.prefix.length + 1, source.length - 1).trim();
+        return Transformer.transformObject(await new Expression(expr).apply(expressionParams), expressionParams);
+      }
+    }
+    return source;
+  }
 }
