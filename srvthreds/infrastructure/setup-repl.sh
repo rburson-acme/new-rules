964352af6ab6cdeddb90588951d03cbdf293ac82
alanresha <aresha@acmegeneral.com>
2025-09-30 09:34:18 -0500

    chore: tweak command to run script

--- srvthreds/infrastructure/setup-repl.sh
+++ srvthreds/infrastructure/setup-repl.sh
@@ -15,7 +15,9 @@ sleep 5
 #     ]
 #   });
 # "
-mongosh "mongodb://localhost:27017" --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'localhost:27017' }] })"
+# mongosh "mongodb://localhost:27017" --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'localhost:27017' }] })"
+
+docker exec mongo-repl-1 mongosh "mongodb://localhost:27017" --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'localhost:27017' }] })"
 
 # mongosh "mongodb://localhost:27017" --eval "
 #   rs.initiate({
